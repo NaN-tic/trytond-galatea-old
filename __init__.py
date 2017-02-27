7e4b871b13b8ab42627b6493731b8b6012ef0820
Raimon Esteve <raimon@nan-tic.com>
2017-02-28 00:17:17 +0100

    Allow Galatea Attachments

--- __init__.py
+++ __init__.py
@@ -2,13 +2,18 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from .attachment import *
 from .galatea import *
 from .configuration import *
 from .static_file import *
 from .party import *
+from .sale import *
+from .invoice import *
+from .project import *
 
 def register():
     Pool.register(
+        Attachment,
         Configuration,
         GalateaWebSite,
         GalateaWebsiteCountry,
@@ -21,6 +26,9 @@ def register():
         GalateaStaticFolder,
         GalateaStaticFile,
         Party,
+        Sale,
+        Invoice,
+        Work,
         module='galatea', type_='model')
     Pool.register(
         GalateaRemoveCache,
